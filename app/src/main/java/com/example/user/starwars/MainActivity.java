740c8c4c2831a7ed8993c1da0847186dfc00b027
Paweł Czarnik <pawelczarnik@interia.pl>
2016-07-14 12:16:37 +0200

    podmiana fragmentow

--- app/src/main/java/com/example/user/starwars/MainActivity.java
+++ app/src/main/java/com/example/user/starwars/MainActivity.java
@@ -1,82 +1,96 @@
 package com.example.user.starwars;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.StringRes;
+import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
-import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.example.user.starwars.mvp.contract.PeopleListContract;
-import com.example.user.starwars.mvp.presenter.PeopleListPresenter;
-
-import java.util.List;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
-public class MainActivity extends AppCompatActivity implements PeopleAdapter.PeopleClickListener, PeopleListContract.View {
-
-
-    @BindView(R.id.peopleRecycleView)
-    RecyclerView peopleRecycleView;
-
-    private PeopleListContract.Presenter presenter;
-    private PeopleAdapter adapter;
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        presenter = new PeopleListPresenter(this, this);
-        ButterKnife.bind(this);
-        peopleRecycleView.setLayoutManager(new LinearLayoutManager(this));
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT) {
-            @Override
-            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
-                return true;
-            }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
-            @Override
-            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-                int position = viewHolder.getAdapterPosition();
-                adapter.notifyDataSetChanged();
-            }
-        };
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-        ItemTouchHelper touchHelper = new ItemTouchHelper(simpleCallback);
-        touchHelper.attachToRecyclerView(peopleRecycleView);
 
-        presenter.getData();
     }
 
     @Override
-    public void onPersonClick(Person person) {
-        Intent details = new Intent(this, DetailsActivity.class);
-        details.putExtra("Person", person);
-        startActivity(details);
-    }
-
-    @Override
-    public void onDataLoaded(List<Person> items) {
-        ensureAdapter(items);
-    }
-
-    @Override
-    public void onErrorOccured(@StringRes int errorMessage) {
-        Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
-    }
-
-    private void ensureAdapter(List<Person> items) {
-        if (adapter == null) {
-            adapter = new PeopleAdapter(items);
-            adapter.setOnClickListener(this);
-            peopleRecycleView.setAdapter(adapter);
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
         } else {
-            adapter.setItems(items);
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.containerLayout, getFragmentForPage(id))
+                .commit();
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    private Fragment getFragmentForPage(@IdRes int id) {
+        if (id == R.id.nav_people) {
+            return PeopleFragment.newInstance();
+        } else if (id == R.id.nav_planets) {
+            return PeopleFragment.newInstance();
+        } else if (id == R.id.nav_starships) {
+            return PeopleFragment.newInstance();
+        } else {
+            throw new IllegalArgumentException("Invalid page ID");
         }
     }
 
--- app/src/main/java/com/example/user/starwars/PeopleFragment.java
+++ app/src/main/java/com/example/user/starwars/PeopleFragment.java
@@ -4,20 +4,32 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.user.starwars.mvp.contract.PeopleListContract;
+import com.example.user.starwars.mvp.presenter.PeopleListPresenter;
 
 import java.util.List;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import timber.log.Timber;
 
 /**
  * Created by user on 14.07.2016.
  */
 public class PeopleFragment extends Fragment implements PeopleListContract.View {
+
+    @BindView(R.id.peopleRecycleView)
+    RecyclerView peopleRecycleView;
+
+    private PeopleListContract.Presenter presenter;
+    private PeopleAdapter adapter;
 
     public static PeopleFragment newInstance() {
         Bundle args = new Bundle();
@@ -30,6 +42,7 @@ public class PeopleFragment extends Fragment implements PeopleListContract.View 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
 
@@ -38,6 +51,9 @@ public class PeopleFragment extends Fragment implements PeopleListContract.View 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_people_list, container, false);
         ButterKnife.bind(this,view);
+        presenter = new PeopleListPresenter(this, getContext());
+        peopleRecycleView.setLayoutManager(new LinearLayoutManager(getContext()));
+        presenter.getData();
         return view;
     }
 
@@ -51,11 +67,21 @@ public class PeopleFragment extends Fragment implements PeopleListContract.View 
 
     @Override
     public void onDataLoaded(List<Person> items) {
-
+        ensureAdapter(items);
     }
 
     @Override
     public void onErrorOccured(@StringRes int errorMessage) {
+        Timber.e("blad");
+    }
 
+    private void ensureAdapter(List<Person> items) {
+        if (adapter == null) {
+            adapter = new PeopleAdapter(items);
+            //adapter.setOnClickListener(this);
+            peopleRecycleView.setAdapter(adapter);
+        } else {
+            adapter.setItems(items);
+        }
     }
 }
